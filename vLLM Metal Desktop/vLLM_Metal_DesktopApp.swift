7e344243ebd2beb45c8243d9c46b95b1d65464fa
RickyChen / 陳昭儒 <ricky.chen@infinirc.com>
2026-06-21 15:54:20 +0800

    Initial Commit

---
+++ vLLM Metal Desktop/vLLM_Metal_DesktopApp.swift
@@ -0,0 +1,17 @@
+//
+//  vLLM_Metal_DesktopApp.swift
+//  vLLM Metal Desktop
+//
+//  Created by RickyChen on 2026/6/21.
+//
+
+import SwiftUI
+
+@main
+struct vLLM_Metal_DesktopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
